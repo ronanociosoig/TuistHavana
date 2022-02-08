bb9dfc8f44db17e25453052806f006dfc86d605c
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2022-02-08 23:28:41 +0100

    Fix folder name

--- Fixtures/AppWithSwiftPackages/Targets/AppWithSwiftPackages/Sources/AppDelegate.swift
+++ Fixtures/AppWithSwiftPackages/Targets/AppWithSwiftPackages/Sources/AppDelegate.swift
@@ -1,6 +1,4 @@
 import UIKit
-import HavanaFixturesKit
-import HavanaFixturesUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         viewController.view.backgroundColor = .white
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
-        HavanaFixturesKit.hello()
-        HavanaFixturesUI.hello()
 
         return true
     }
-
 }
