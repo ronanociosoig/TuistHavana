1255d7fb42b0529de00b181959189e1989cd263d
Ronan O Ciosoig <ronan.ociosoig@edreamsodigeo.com>
2022-04-09 13:27:32 +0200

    Failing dependencies are working with 3.1.0

--- Fixtures/AppWithFailingSPMDependencies/Targets/AppWithFailingSPMDependencies/Sources/AppDelegate.swift
+++ Fixtures/AppWithFailingSPMDependencies/Targets/AppWithFailingSPMDependencies/Sources/AppDelegate.swift
@@ -1,6 +1,4 @@
 import UIKit
-import AppWithFailingSPMDependenciesKit
-import AppWithFailingSPMDependenciesUI
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,10 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         viewController.view.backgroundColor = .white
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
-        AppWithFailingSPMDependenciesKit.hello()
-        AppWithFailingSPMDependenciesUI.hello()
-
         return true
     }
-
 }
